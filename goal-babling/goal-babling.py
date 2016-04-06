1f08800a3d51998dfe3e9b97afea8060b62caa31
Tytam <mva5555@gmail.com>
2016-04-06 09:15:55 +0200

    Goal babling
    results in zaujimavosti

--- goal-babling/goal-babling.py
+++ goal-babling/goal-babling.py
@@ -14,7 +14,8 @@ class senso_motor_pair:
 		self.joints = [None] * 7
 		return
 	def __init__(self, ID, j):
-		self.skin_cube_ID = ID.split()
+		print ID
+		self.skin_cube_ID = str(ID.split())
 		self.joints = copy.deepcopy(j);
 		return
 	
@@ -37,6 +38,44 @@ citac = ExampleReader()
 #print citac.getData()
 
 model = []
+distmat = distancematrix()
+
+def preklad(index):
+	if index == 0:
+		return "11"
+	elif index == 1:
+		return "12"
+	elif index == 2:
+		return "13"
+	elif index == 3:
+		return  "21"
+	elif index == 4:
+		return  "22"
+	elif index == 5:
+		return  "23"
+	elif index == 6:
+		return  "24"
+	elif index == 7:
+		return  "25"
+	elif index == 8:
+		return  "26"
+	elif index == 9:
+		return  "27"
+	elif index == 10:
+		return  "28"	
+
+def getaveragemotor(index):
+	pocet = 0
+	suma = [0,0,0,0,0,0,0]
+	for item in model:
+		if preklad(index) in item.skin_cube_ID:
+			pocet+=1 
+			for i in range(0, 7):
+				suma[i] += item.joints[i]
+	for i in range(0, 6):
+		suma[i] = suma[i] / pocet
+	return suma
+				 
 
 #prepare pasive hand
 left = left_arm_init()
@@ -90,7 +129,7 @@ tmp.set(14, 154)
 tmp.set(15, 167)
 
 hom=yarp.Vector(jnts)
-hom.set(1, 15)
+hom.set(1, -80)
 hom.set(2, 40)
 hom.set(3, 70)
 hom.set(4, 20)
@@ -149,7 +188,8 @@ while semafor:
 	#print "zaspal"
 	time.sleep(3)
 	dat = citac.getData()
-	print dat 
+	print dat
+	dat = str(dat) 
 	if dat != "":
 		model.append(senso_motor_pair(dat, joint_setting))
 		d = dat.split()
@@ -180,11 +220,13 @@ while semafor:
 		suma = 0
 		for j in range(0, 10):
 			suma += zname[j]
-		if suma > 1:
-			for k in range(0, 10)
-				kamarati = getkamaratov(self, k, zname)
+		if suma > 2:
+			for k in range(0, 10):
+				kamarati = distmat.getkamaratov(k, zname)
 				if len(kamarati) > 1:
-					semafor = False
+					print kamarati
+					if len(model) > 1:
+						semafor = False
 				
 	
 	#print "zobudeny"
@@ -193,11 +235,115 @@ while semafor:
 	print x
 
 
+semafor = True
+while semafor:
+	#vyberieme nechytenu kocku s aspon 2 znamimi kamaratmi
+	vybrana = False
+	i = -1
+	kamaratiindexy = []
+	while vybrana == False:
+		i+=1
+		kamaratiindexy = distmat.getkamaratov(i, zname)
+		if len(kamaratiindexy) > 1 and zname[i] == 0:
+			vybrana = True
 
+	print "hladame na indexe " + str(i)
+	kamarati = []
+	for item in kamaratiindexy:
+		kamarati.append(getaveragemotor(item))
+	
+	#urcime zonu prehladavania a prehladavame
 
+	x = 0
+	while x < 30:
+		x += 1
+		pole = []
+		for item in kamarati:
+			pole.append(item[0])		
+		r = random.uniform(min(pole), max(pole))
+		tmp.set(0, r)
+		joint_setting[0] = r
+		pole = []
+		for item in kamarati:
+			pole.append(item[1])
+		r = random.uniform(min(pole), max(pole))
+		tmp.set(1, r)
+		joint_setting[1] = r
+		pole = []
+		for item in kamarati:
+			pole.append(item[2])
+		r = random.uniform(min(pole), max(pole))
+		joint_setting[2] = r
+		tmp.set(2, r)
+		pole = []
+		for item in kamarati:
+			pole.append(item[3])
+		r = random.uniform(min(pole), max(pole))
+		joint_setting[3] = r
+		tmp.set(3, r)
+		pole = []
+		for item in kamarati:
+			pole.append(item[4]) 
+		r = random.uniform(min(pole), max(pole))
+		joint_setting[4] = r
+		tmp.set(4, r) 
+		pole = []
+		for item in kamarati:
+			pole.append(item[5])   
+		r = random.uniform(min(pole), max(pole))
+		joint_setting[5] = r
+		tmp.set(5, r)
+		pole = []
+		for item in kamarati:
+			pole.append(item[6]) 
+		r = random.uniform(min(pole), max(pole))
+		joint_setting[6] = r
+		tmp.set(6, r) 
+		iPos.positionMove(tmp.data())
+		#print "zaspal"
+		time.sleep(3)
+		dat = citac.getData()
+		print dat
+		dat = str(dat)  
+		if dat != "":
+			model.append(senso_motor_pair(dat, joint_setting))
+			d = dat.split()
+			for item in d:
+	    			if item == "11":
+					zname[0] = 1
+				if item == "12":
+					zname[1] = 1
+				if item == "13":
+					zname[2] = 1
+				if item == "21":
+					zname[3] = 1
+				if item == "22":
+					zname[4] = 1
+				if item == "23":
+					zname[5] = 1
+				if item == "24":
+					zname[6] = 1
+				if item == "25":
+					zname[7] = 1
+				if item == "26":
+					zname[8] = 1
+				if item == "27":
+					zname[9] = 1
+				if item == "28":
+					zname[10] = 1
+		iPos.positionMove(hom.data())
+		time.sleep(2)
+		print x
 
-
-
+		 
+	
+	#nasli sme 8/10 prehladavanie konci
+	suma = 0
+	for j in range(0, 10):
+		suma += zname[j]
+	if suma > 7:
+		semafor = False
+	
 
 
 
